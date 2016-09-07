de2df8b52bf40d4e3c76a4d6b3a6f25c86c74909
Amar Agrawal <amar.agrawal@revx.io>
2016-09-07 12:10:06 +0530

    adding code to fetch from cache

--- sw.js
+++ sw.js
@@ -39,6 +39,20 @@ self.addEventListener('install', function(event) {
   console.log('Installed', event);
 });
 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
 self.addEventListener('activate', function(event) {
   console.log('Activated', event);
 });
